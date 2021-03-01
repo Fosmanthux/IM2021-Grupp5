f107940436b7a4c413ecf8add53f0a8c2c204135
vanessachanel <78602207+vanessachanel@users.noreply.github.com>
2021-03-01 12:29:44 +0100

    V delkomponent
    Vanessas delkomponent

---
+++ New Unity Project/Assets/Vanessas delkomponent/Assets/Scripts/LightEstimaiton.cs
@@ -0,0 +1,73 @@
+
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR.ARFoundation;
+
+public class LightEstimaiton : MonoBehaviour
+{
+    [SerializeField]
+    private ARCameraManager arCameraManager;
+
+    [SerializeField]
+
+    private Text brightnessValue;
+
+    [SerializeField]
+
+    private Text tempValue;
+
+   [SerializeField]
+
+    private Text colorCorrectionValue;
+
+    private Light currentLight;
+
+    private void Awake()
+    {
+        currentLight = GetComponent<Light>();
+    }
+
+
+    private void OnEnable()
+    {
+        arCameraManager.frameReceived += FrameUpdated;
+    }
+
+    private void OnDisable()
+    {
+        arCameraManager.frameReceived -= FrameUpdated;
+    }
+
+    private void FrameUpdated(ARCameraFrameEventArgs args)
+
+    {
+
+        if(args.lightEstimation.averageBrightness.HasValue)
+
+        {
+            brightnessValue.text = $"Brightness: {args.lightEstimation.averageBrightness.Value}";
+            currentLight.intensity = args.lightEstimation.averageBrightness.Value;
+        }
+
+        if(args.lightEstimation.averageColorTemperature.HasValue)
+
+        {
+
+            tempValue.text = $"Temp: {args.lightEstimation.averageColorTemperature.Value}";
+            currentLight.colorTemperature = args.lightEstimation.averageColorTemperature.Value;
+        }
+
+
+        if(args.lightEstimation.colorCorrection.HasValue)
+
+        {
+
+            colorCorrectionValue.text = $"Color: {args.lightEstimation.colorCorrection.Value}";
+            currentLight.color = args.lightEstimation.colorCorrection.Value;
+
+        }
+    }
+
+
+
+}
