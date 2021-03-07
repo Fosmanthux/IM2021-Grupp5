d912348a0bb09fbe1c849089e70c0b7259a64ec4
ZahraMoghimi <78591263+ZahraMoghimi@users.noreply.github.com>
2021-03-08 00:17:29 +0100

    ny

---
+++ New Unity Project/Assets/MainMenu.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void start()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void quit()
+    {
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+        Debug.Log("Quit!");
+        Application.Quit(); 
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("Menu"); 
+    }
+    public void LoadOptionsScene()
+    {
+        SceneManager.LoadScene("Options"); 
+    }
+}
